0a01ca04dd0d6341fd1ede9eb8df6cd99f551590
root <root@Mark-II.(none)>
2014-12-02 20:28:25 +0530

    customize product listing onhome page

---
+++ app/controllers/spree/home_controller_decorator.rb
@@ -0,0 +1,11 @@
+module HomeControllerExtensions
+	def index
+		@searcher = build_searcher(params.merge(include_images: true))
+		@products = @searcher.retrieve_products
+		@taxonomies = Spree::Taxonomy.includes(root: :children)
+	end
+end
+
+Spree::HomeController.class_eval do
+  prepend HomeControllerExtensions
+end
