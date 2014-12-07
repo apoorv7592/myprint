64437f3e9f13d76d65c7140109b9020a7416cc55
“apoorv7592git config --global user.email “apoorviiti@gmail.com <Appycommits>
2014-12-07 23:36:39 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   Spree::Core::Engine.routes.prepend do
-    get 'suites/:id' => 'suites#show'
-  end
+     get '/suites/:id' => 'suites#show'  end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
