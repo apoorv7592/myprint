b22c41c8e98208673530b63523c462b28513763b
root <root@Mark-II.(none)>
2015-02-15 11:12:11 +0530

    delete functionality added to suites admin

--- app/controllers/spree/admin/suites_controller.rb
+++ app/controllers/spree/admin/suites_controller.rb
@@ -4,6 +4,11 @@ module Spree
 			def index
 				@suites = Suite.all
 			end
+
+			def show
+				@suite = Suite.where(id: params[:id]).first	
+			end
+
 			def new
 				@suite = Suite.new
 			end
@@ -19,6 +24,32 @@ module Spree
 						format.html {redirect_to :back , notice: 'Some error occured'}
 						format.js {@msg = 'Some error occured'}
 					end
+				end
+			end
+
+			def edit
+				@suite = Suite.where(params[:id]).first
+			end
+
+			def update
+				@s = Suite.where(params[:id]).first
+				if @s.update(suites_params)
+					respond_to do |format|
+						format.html {redirect_to admin_suite_path(@s)}
+					end
+				else
+					respond_to do |format|
+						format.html {redirect_to :back , notice: 'Some error occured'}
+					end
+				end
+			end
+
+			def destroy
+				@suite = Suite.where(id: params[:id]).first
+				if @suite.destroy
+					redirect_to admin_suites_path, message:'Suite deleted successfully'
+				else
+					redirect_to :back, message:'Some error occured'
 				end
 			end
 
