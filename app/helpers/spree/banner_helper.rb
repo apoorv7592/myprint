3032505850f9d04b849c58d20f02918df30d6051
root <root@Mark-II.(none)>
2015-02-22 14:19:28 +0530

    bug fix of banners

--- app/helpers/spree/banner_helper.rb
+++ app/helpers/spree/banner_helper.rb
@@ -1,9 +1,8 @@
 module Spree
 	module BannerHelper
 		def get_banner(category, position)
-			
 			if category && position
-				
+				Banner.where(category: category, position: position)
 			else
 				"Banner Category or position not supplied"
 			end
