bb5ba302a6f3b99814942a075ab29ef408af999f
root <root@Mark-II.(none)>
2015-03-22 17:26:23 +0530

    remove variant migrations"

--- db/migrate/20150319063810_add_variant_to_suites.rb
+++
@@ -1,5 +0,0 @@
-class AddVariantToSuites < ActiveRecord::Migration
-  def change
-  add_column :suites, :variant, :string
-  end
-end
--- db/migrate/20150319140411_add_colorcode_to_variants.rb
+++
@@ -1,5 +0,0 @@
-class AddColorcodeToVariants < ActiveRecord::Migration
-  def change
-    add_column :variants, :colorcode, :string
-  end
-end
