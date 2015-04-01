c9c176ba4e284c53d046952ec8fddefb6b5a3d50
root <root@Mark-II.(none)>
2015-04-02 01:22:00 +0530

    suite images

--- app/models/suite.rb
+++ app/models/suite.rb
@@ -35,7 +35,7 @@ class Suite < ActiveRecord::Base
 	has_attached_file :avatar,  styles: {
     thumb: '100x100>',
     square: '200x200#',
-    medium: '300x300>',
+    medium: '238x238>',
     large:  '500x500>'
     }
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/  # Validate the attached image is image/jpg, image/png, etc	
