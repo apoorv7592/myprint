08dc37cc652e9d5fd1eb7c0e6c88204cd5eecec7
“apoorv7592git config --global user.email “apoorviiti@gmail.com <Appycommits>
2015-04-13 19:09:50 +0530

    suite page

--- app/controllers/spree/shop_controller.rb
+++ app/controllers/spree/shop_controller.rb
@@ -1,33 +1,54 @@
 module Spree
-	class ShopController < Spree::HomeController
-		
-		def wedding
-			@subcat = SubCategory.friendly.find(params[:id])
-			sub_cat_id = @subcat.id
-			#search(q,sub_cat_id,designer, color_id, trim_id,dimension_id, created_at,like_no,avg_rating,price )
-			@search = Suite.search(params[:search], sub_cat_id, params[:designer],
-				params[:color_id], params[:trim_id], params[:dimension_id],
-				params[:created_at], params[:like_no], params[:avg_rating], params[:price])
-		    
-		    @scsuite = @search.results
-		end
-		def greeting
-			@subcat = SubCategory.friendly.find(params[:id])
-			sub_cat_id = @subcat.id
-			@search = Suite.search(params[:search], sub_cat_id, params[:designer],
-				params[:color_id], params[:trim_id], params[:dimension_id],
-				params[:created_at], params[:like_no], params[:avg_rating], params[:price])
+    class ShopController < Spree::HomeController
+        
+        def wedding
+            par = params[:id]
+            if par=='shop-all'
+                @search = Suite.search(params[:search], params[:sub_cat_id], params[:designer],
+                    params[:color_id], params[:trim_id], params[:dimension_id],
+                    params[:created_at], params[:like_no], params[:avg_rating], params[:price], 'Wedding Invitations')
+            else
+                @subcat = SubCategory.friendly.find(params[:id])
+                sub_cat_id = @subcat.id
+                #search(q,sub_cat_id,designer, color_id, trim_id,dimension_id, created_at,like_no,avg_rating,price )
+                @search = Suite.search(params[:search], sub_cat_id, params[:designer],
+                    params[:color_id], params[:trim_id], params[:dimension_id],
+                    params[:created_at], params[:like_no], params[:avg_rating], params[:price], params[:category_id])
+            end
+            @scsuite = @search.results
+        end
+        def greeting
+            par = params[:id]
+            if par=='shop-all'
+                @search = Suite.search(params[:search], params[:sub_cat_id], params[:designer],
+                    params[:color_id], params[:trim_id], params[:dimension_id],
+                    params[:created_at], params[:like_no], params[:avg_rating], params[:price], 'Other Invitations')
+            else
+                @subcat = SubCategory.friendly.find(params[:id])
+                sub_cat_id = @subcat.id
+                #search(q,sub_cat_id,designer, color_id, trim_id,dimension_id, created_at,like_no,avg_rating,price )
+                @search = Suite.search(params[:search], sub_cat_id, params[:designer],
+                    params[:color_id], params[:trim_id], params[:dimension_id],
+                    params[:created_at], params[:like_no], params[:avg_rating], params[:price], params[:category_id])
+            end
+            @scsuite = @search.results
+        end
+        def otherinvites
+            par = params[:id]
+            if par=='shop-all'
+                @search = Suite.search(params[:search], params[:sub_cat_id], params[:designer],
+                    params[:color_id], params[:trim_id], params[:dimension_id],
+                    params[:created_at], params[:like_no], params[:avg_rating], params[:price], 'Other Invitations')
+            else
+                @subcat = SubCategory.friendly.find(params[:id])
+                sub_cat_id = @subcat.id
+                #search(q,sub_cat_id,designer, color_id, trim_id,dimension_id, created_at,like_no,avg_rating,price )
+                @search = Suite.search(params[:search], sub_cat_id, params[:designer],
+                    params[:color_id], params[:trim_id], params[:dimension_id],
+                    params[:created_at], params[:like_no], params[:avg_rating], params[:price], params[:category_id])
+            end
+            @scsuite = @search.results
+        end
 
-		    @scsuite = @search.results
-		end
-		def partyinvites
-			@subcat = SubCategory.friendly.find(params[:id])
-			sub_cat_id = @subcat.id
-			@search = Suite.search(params[:search], sub_cat_id, params[:designer],
-				params[:color_id], params[:trim_id], params[:dimension_id],
-				params[:created_at], params[:like_no], params[:avg_rating], params[:price])			
-		    @scsuite = @search.results
-		end
-
-	end
+    end
 end
