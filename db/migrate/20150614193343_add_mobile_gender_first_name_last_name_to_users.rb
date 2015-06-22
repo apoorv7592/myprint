e909d6b44ccdcdb0c4dae32188d1832496c27c07
root <root@Mark-II.(none)>
2015-06-22 14:40:05 +0530

    remove name rename migration

--- db/migrate/20150614193343_add_mobile_gender_first_name_last_name_to_users.rb
+++ db/migrate/20150614193343_add_mobile_gender_first_name_last_name_to_users.rb
@@ -1,6 +1,6 @@
 class AddMobileGenderFirstNameLastNameToUsers < ActiveRecord::Migration
   def change
-  	rename_column :spree_users, :name, :first_name
+  	add_column :spree_users, :first_name, :string
   	add_column :spree_users, :last_name, :string
   	add_column :spree_users, :phone, :string
   	add_column :spree_users, :gender, :string
