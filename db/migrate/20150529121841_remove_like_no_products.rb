bc4862f96543b6502687a3d157e4c3e186681a95
root <root@Mark-II.(none)>
2015-06-22 14:32:44 +0530

    migration bug fix

--- db/migrate/20150529121841_remove_like_no_products.rb
+++ db/migrate/20150529121841_remove_like_no_products.rb
@@ -1,6 +1,6 @@
 class RemoveLikeNoProducts < ActiveRecord::Migration
   def change
-  	remove_column :spree_products, :like_no
+  	#remove_column :spree_products, :like_no
   	add_column :spree_products, :like_no, :integer, default: 0
   end
 end
